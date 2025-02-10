3afe548d51408435ba879aab42aa23467e660c7d
disa4148 <denis.nikolaenko.2004@mail.ru>
2025-02-10 23:51:08 +0300

    feat: side-bar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { SubscriberCardComponent } from "./subscriber-card/subscriber-card.component";
+
+type MenuItems = {
+  label: string,
+  icon: string,
+  link: string,
+}
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [SvgIconComponent, SubscriberCardComponent],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
-export class SideBarComponent {
 
+export class SideBarComponent {
+  menuItems: MenuItems[] = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '/profile'
+    },
+    {
+      label: 'Чаты',
+      icon: 'message',
+      link: '/chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: '/'
+    }
+  ]
 }
---
+++ src/app/components/side-bar/subscriber-card/subscriber-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+// import { Subscriber } from '../../../data/interfaces/subscriber.interface';
+
+@Component({
+  selector: 'app-subscriber-card',
+  imports: [],
+  templateUrl: './subscriber-card.component.html',
+  styleUrl: './subscriber-card.component.scss'
+})
+export class SubscriberCardComponent {
+  // @Input() subscriber!: Subscriber
+}
