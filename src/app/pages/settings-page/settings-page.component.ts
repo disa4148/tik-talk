66195122a6fe3b2c2549db22e653e89b32daf586
disa4148 <denis.nikolaenko.2004@mail.ru>
2025-02-13 21:42:15 +0300

    feat(settings): skill addition form

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, inject, effect } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ProfileService } from '../../data/services/profile.service';
 import { firstValueFrom } from 'rxjs';
@@ -8,11 +8,20 @@ import { Profile } from '../../data/interfaces/profile.interface';
   selector: 'app-settings-page',
   imports: [ReactiveFormsModule],
   templateUrl: './settings-page.component.html',
-  styleUrl: './settings-page.component.scss'
+  styleUrl: './settings-page.component.scss',
 })
+
 export class SettingsPageComponent {
   fb = inject(FormBuilder);
   profileService = inject(ProfileService);
+
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: [{ value: '', disabled: true }, Validators.required],
+    description: ['', Validators.required],
+    stack: [[] as string[]],
+  });
 
   constructor() {
     effect(() => {
@@ -20,16 +29,8 @@ export class SettingsPageComponent {
       if (profile) {
         this.form.patchValue(profile);
       }
-    })
+    });
   }
-
-  form = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    username: [{value: '', disabled: true}, Validators.required],
-    description: ['', Validators.required],
-    stack: [[] as string[]],
-  })
 
   onSubmit() {
     this.form.markAllAsTouched();
@@ -37,6 +38,31 @@ export class SettingsPageComponent {
 
     if (this.form.invalid) return;
 
-    firstValueFrom(this.profileService.patchProfile(this.form.value as Partial<Profile>));
+    firstValueFrom(
+      this.profileService.patchProfile(this.form.value as Partial<Profile>)
+    );
+  }
+
+  addSkill(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement | null;
+    if (!input) return;
+
+    const value = input.value.trim();
+
+    if ((event.key === 'Enter' || event.key === ',') && value) {
+
+      const currentStack = this.form.value.stack || [];
+      if (!currentStack.includes(value)) {
+        this.form.patchValue({ stack: [...currentStack, value] });
+      }
+
+      input.value = '';
+    }
+  }
+
+  removeSkill(skill: string) {
+    const updatedStack =
+      this.form.value.stack?.filter((s) => s !== skill) || [];
+    this.form.patchValue({ stack: updatedStack });
   }
 }
